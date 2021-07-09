05405155959fa7d0372232e8731a52726bdd8248
SunshineTanKuKu <42287246+tankokkeng@users.noreply.github.com>
2021-07-09 22:26:51 +0800

    Add files via upload

---
+++ Generate ServerText.ps1
@@ -0,0 +1,6 @@
+﻿$computers = Get-ADComputer -Filter 'OperatingSystem -like "*Serv*"' -Properties Name,LastLogonDate | Where { $_.LastLogonDate -GT (Get-Date).AddDays(-30) }
+foreach ($computer in $computers)
+{
+    $computer.Name | Out-File -Append C:\scripts\servers.txt
+}
+
